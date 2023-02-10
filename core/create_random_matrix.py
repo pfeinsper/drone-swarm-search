8fa02404842d56266f4cec380a20bb6c0da4f4dd
Enrico <enrico.francesco.damiani@gmail.com>
2023-02-10 11:51:28 -0300

    Delete comment

--- core/create_random_matrix.py
+++ core/create_random_matrix.py
@@ -28,7 +28,6 @@ def divide_number(number: int, parts_number: int) -> list:
 def create_random_matrix(size_x: int, size_y: int) -> np.matrix:
     temporary_matrix = []
     probability_list = divide_number(size_x * size_y * 10, size_x * size_y)
-    # Pq mult por 10?
     counter = 0
 
     for _ in range(size_x):
