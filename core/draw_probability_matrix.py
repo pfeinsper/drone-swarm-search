3255fda71d90fff6a4b6e145c19fbf967fc1f838
Enrico <enrico.francesco.damiani@gmail.com>
2023-02-10 11:46:46 -0300

    fixes

--- core/draw_probability_matrix.py
+++ core/draw_probability_matrix.py
@@ -1,15 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from create_random_matrix import create_random_matrix
 
 
-def draw_matrix(matrix):
+def draw_matrix(matrix: np.matrix):
     length = len(matrix)
-    fig, ax = plt.subplots()
+    _, ax = plt.subplots()
+
     for i in range(length):
+        
         for j in range(length):
-            text = ax.text(
-                j, i, matrix[i][j], ha="center", va="center", color="w"
+            _ = ax.text(
+                j, i, matrix[i, j], ha="center", va="center", color="w"
             )
-    # ax.matshow(matrix, cmap=plt.cm.Spectral)
+
     ax.matshow(matrix)
     plt.show()
+
+
+draw_matrix(create_random_matrix(10, 10))
