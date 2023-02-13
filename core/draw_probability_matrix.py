8a845935af1b00343ef3cef7f674bc53531ce05e
leonardo.malta <leonardo.malta@torqi.com.br>
2023-02-13 11:10:45 -0300

    code formatting

--- core/draw_probability_matrix.py
+++ core/draw_probability_matrix.py
@@ -1,16 +1,14 @@
 import matplotlib.pyplot as plt
 from numpy import array
 
+
 def draw_matrix(matrix: array):
     length = len(matrix)
     _, ax = plt.subplots()
 
     for i in range(length):
-        
         for j in range(length):
-            _ = ax.text(
-                j, i, matrix[i, j], ha="center", va="center", color="w"
-            )
+            _ = ax.text(j, i, matrix[i, j], ha="center", va="center", color="w")
 
     ax.matshow(matrix)
     plt.show()
