0e90d99ad8fe298eafcb38637f99aa24aa44d807
leonardo.malta <leonardo.malta@torqi.com.br>
2023-02-14 20:17:39 -0300

    calculate_diagonal_movements working

--- core/calculate_array_movements.py
+++ core/calculate_array_movements.py
@@ -1,5 +1,7 @@
-from typing import Tuple, List
+from typing import List, Tuple
 from enum import Enum
+
+POINT_TYPE = Tuple[int, int]
 
 
 class Movements(Enum):
@@ -14,8 +16,53 @@ class Movements(Enum):
     SEARCH = "SEARCH"
 
 
-def calculate_movements(movements_sequence: List[Tuple[int, int]]) -> List[Movements]:
+diagonal_translator = {
+    (Movements.UP, Movements.LEFT): (Movements.DIOGONAL_UP_LEFT, (-1, -1)),
+    (Movements.UP, Movements.RIGHT): (Movements.DIOGONAL_UP_RIGHT, (-1, 1)),
+    (Movements.DOWN, Movements.LEFT): (Movements.DIOGONAL_DOWN_LEFT, (1, -1)),
+    (Movements.DOWN, Movements.RIGHT): (Movements.DIOGONAL_DOWN_RIGHT, (1, 1)),
+}
+
+
+def calculate_diagonal_movements(
+    current_point: POINT_TYPE, next_point: POINT_TYPE
+) -> Tuple[List[Movements], POINT_TYPE]:
+    """Calculate and return the diogonal movements and current position."""
+
+    distance_line = next_point[0] - current_point[0]
+    distance_column = next_point[1] - current_point[1]
+
+    movement_line = Movements.DOWN if distance_line > 0 else Movements.UP
+    movement_column = Movements.RIGHT if distance_column > 0 else Movements.LEFT
+    diagonal_movement, points_to_move = diagonal_translator[
+        (movement_line, movement_column)
+    ]
+
+    number_of_movements = min(abs(distance_line), abs(distance_column))
+    movements = [diagonal_movement] * number_of_movements
+    position_after_movements = (
+        current_point[0] + points_to_move[0] * number_of_movements,
+        current_point[1] + points_to_move[1] * number_of_movements,
+    )
+
+    return (movements, position_after_movements)
+
+
+def calculate_path(
+    current_point: POINT_TYPE, next_point: POINT_TYPE
+) -> List[Movements]:
+    """Calculate the path between two points."""
+    path: List[Movements] = []
+    calculate_diagonal_movements(current_point, next_point)
+
+    return path
+
+
+def calculate_movements(movements_sequence: List[POINT_TYPE]) -> List[Movements]:
     """Calculate movements from a sequence of coordinates."""
     movements: List[Movements] = []
+
+    for current_point, next_point in zip(movements_sequence, movements_sequence[1:]):
+        path_to_next_point = calculate_path(current_point, next_point)
 
     return movements
--- tests/test_calculate_array_movements.py
+++ tests/test_calculate_array_movements.py
@@ -1,6 +1,42 @@
 import pytest
 
-from core.calculate_array_movements import calculate_movements, Movements
+from core.calculate_array_movements import (
+    calculate_movements,
+    calculate_diagonal_movements,
+    Movements,
+)
+
+
+@pytest.mark.parametrize(
+    "current_point, next_point, expected_movements, expected_position",
+    [
+        (
+            (4, 4),
+            (0, 1),
+            [
+                Movements.DIOGONAL_UP_LEFT,
+                Movements.DIOGONAL_UP_LEFT,
+                Movements.DIOGONAL_UP_LEFT,
+            ],
+            (1, 1),
+        ),
+        (
+            (4, 4),
+            (0, 5),
+            [
+                Movements.DIOGONAL_UP_RIGHT,
+            ],
+            (3, 5),
+        ),
+    ],
+)
+def test_calculate_diagonal_movements(
+    current_point, next_point, expected_movements, expected_position
+):
+    """Test the calculate_diagonal_movements function."""
+    movements, position = calculate_diagonal_movements(current_point, next_point)
+    assert movements == expected_movements
+    assert position == expected_position
 
 
 # Mock the movements sequence entries
