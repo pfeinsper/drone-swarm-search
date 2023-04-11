c234955f3d519807fb987423e3261a6cefc86557
Luis Filipe Carrete <lfcsancehz@live.com>
2023-04-11 11:05:40 -0300

    Feat multi agent env

--- core/environment/env.py
+++ core/environment/env.py
@@ -3,25 +3,27 @@ import random
 from copy import copy
 
 import numpy as np
-from gymnasium.spaces import MultiDiscrete
+from gymnasium.spaces import MultiDiscrete, Discrete
 
 from pettingzoo.utils.env import ParallelEnv
 import sys
 
-sys.path.append("../")
-from .generator.probability import generate_probability_matrix
-from .generator.map import generate_map
+from generator.probability import generate_probability_matrix
+from generator.map import generate_map
 
 
 class CustomEnvironment(ParallelEnv):
-    def __init__(self, grid_size=7, render_mode="ansi"):
+    def __init__(self, grid_size=7, render_mode="ansi", n_drones=1):
         self.grid_size = grid_size
         self.person_y = None
         self.person_x = None
-        self.drone_y = None
-        self.drone_x = None
         self.timestep = None
-        self.possible_agents = ["drone"]
+        self.possible_agents = []
+        self.agents_positions = {}
+        for i in range(n_drones):
+            self.possible_agents.append("drone"+str(i))
+            self.agents_positions["drone"+str(i)] = [None, None]
+        
         self.probability_matrix = None
 
         self.render_mode = render_mode
@@ -33,114 +35,128 @@ class CustomEnvironment(ParallelEnv):
 
     def reset(self, seed=None, return_info=False, options=None):
         self.agents = copy(self.possible_agents)
+        
+
         self.timestep = 0
+        counter_x = 0
+        counter_y = 0
+        for i in self.agents:
+            self.agents_positions[i] = [counter_x, counter_y]
+            counter_y += 1
+            counter_x += 1
+        self.agents_positions["drone3"] = [2,2]
+        observations = {}
+        for i in self.agents:
+            
+            observation = (
+                (self.agents_positions[i][0], self.agents_positions[i][1]),
+                self.probability_matrix,
+            )
 
-        self.drone_x = 0
-        self.drone_y = 0
+            observations[i] = {
+                "observation": observation
+            }
 
-        observation = (
-            (self.drone_x, self.drone_y),
-            self.probability_matrix,
-        )
-        observations = {
-            "drone": {"observation": observation, "action_mask": [0, 1, 1, 0, 1]},
-        }
-
-        # self.render_probability_matrix()
         return observations
 
     def step(self, actions):
-        # Execute actions
-        drone_action = actions["drone"]
 
-        # Check termination conditions
         terminations = {a: False for a in self.agents}
         rewards = {a: -1 for a in self.agents}
-        truncations = {"drone": False}
+        truncations = {a: False for a in self.agents}
 
-        isSearching = False
+        for i in self.agents:
+           
+            drone_action = actions[i]
+            # Check termination conditions
+            drone_x = self.agents_positions[i][0]
+            drone_y = self.agents_positions[i][1]
+            
+            isSearching = False
 
-        if drone_action == 0:  # left
-            if self.drone_x > 0:
-                self.drone_x -= 1
-            else:
-                rewards = {"drone": -1000}
-                truncations = {"drone": True}
-                terminations = {"drone": True}
+            if drone_action == 0:  # left
+                if drone_x > 0:
+                    drone_x -= 1
+                else:
+                    rewards[i] = -1000
+                    truncations[i] = True
+                    terminations[i] = True
 
-        elif drone_action == 1:
-            if self.drone_x < self.grid_size - 1:
-                self.drone_x += 1
-            else:
-                rewards = {"drone": -1000}
-                truncations = {"drone": True}
-                terminations = {"drone": True}
-        elif drone_action == 2:  # down
-            if self.drone_y > 0:
-                self.drone_y -= 1
-            else:
-                rewards = {"drone": -1000}
-                truncations = {"drone": True}
-                terminations = {"drone": True}
-        elif drone_action == 3:  # up
-            if self.drone_y < self.grid_size - 1:
-                self.drone_y += 1
-            else:
-                rewards = {"drone": -1000}
-                truncations = {"drone": True}
-                terminations = {"drone": True}
+            elif drone_action == 1:
+                if drone_x < self.grid_size - 1:
+                    drone_x += 1
+                else:
+                    rewards[i] = -1000
+                    truncations[i] = True
+                    terminations[i] = True
+            elif drone_action == 2:  # down
+                if drone_y > 0:
+                    drone_y -= 1
+                else:
+                    rewards[i] = -1000
+                    truncations[i] = True
+                    terminations[i] = True
+            elif drone_action == 3:  # up
+                if drone_y < self.grid_size - 1:
+                    drone_y += 1
+                else:
+                    rewards[i] = -1000
+                    truncations[i] = True
+                    terminations[i] = True
 
-        elif drone_action == 4:  # search
-            isSearching = True
+            elif drone_action == 4:  # search
+                isSearching = True
 
-        # Generate action masks
-        drone_action_mask = np.ones(5)
-        if self.drone_x == 0:
-            drone_action_mask[0] = 0  # Block left movement
-        elif self.drone_x == self.grid_size - 1:
-            drone_action_mask[1] = 0  # Block right movement
-        if self.drone_y == 0:
-            drone_action_mask[2] = 0  # Block down movement
-        elif self.drone_y == self.grid_size - 1:
-            drone_action_mask[3] = 0  # Block up movement
 
-        if (
-            self.drone_x == self.person_x
-            and self.drone_y == self.person_y
-            and isSearching
-        ):
-            rewards = {"drone": 0}
-            terminations = {a: True for a in self.agents}
-            truncations = {a: True for a in self.agents}
-            self.agents = []
+            if (
+                drone_x == self.person_x
+                and drone_y == self.person_y
+                and isSearching
+            ):
+                rewards = {a: 0 for a in self.agents}
+                terminations = {a: True for a in self.agents}
+                truncations = {a: True for a in self.agents}
+                self.agents = []
 
-        elif isSearching:
-            rewards = {
-                "drone": self.probability_matrix[self.drone_y][self.drone_x] - 100
-            }
+            elif isSearching:
+                rewards[i] =  self.probability_matrix[drone_y][drone_x] - 100
+                
 
-        # Check truncation conditions (overwrites termination conditions)
-        if self.timestep > 500:
-            rewards = {"drone": -1000}
-            truncations = {"drone": True}
-            terminations = {"drone": True}
-            self.agents = []
+            # Check truncation conditions (overwrites termination conditions)
+            if self.timestep > 500:
+                rewards[i] = -1000
+                truncations[i] = True
+                terminations[i] = True
+                self.agents = []
         self.timestep += 1
 
         # Get observations
-        observation = (
-            (self.drone_x, self.drone_y),
-            self.probability_matrix,
-        )
-        observations = {
-            "drone": {
-                "observation": observation,
-                "action_mask": drone_action_mask,
-            },
-        }
-        # Get dummy infos (not used in this example)
-        infos = {"drone": {}}
+        observations = {}
+        
+        for i in self.possible_agents:
+            observation = (
+                (self.agents_positions[i][0], self.agents_positions[i][1]),
+                self.probability_matrix,
+            )
 
+            observations[i] = {
+                "observation": observation
+            }
+        # Get dummy infos (not used in this example)
+        infos = {e:{} for e in self.agents}
+        
+        #CHECK COLISION
+        for ki, i in self.agents_positions.items():
+            for ke, e in self.agents_positions.items():
+                if ki is not ke:
+
+                    if(i[0]==e[0] and i[1]==e[1]):
+                        
+                        truncations[ki] = True
+                        terminations[ki] = True
+                        rewards[ki] = -2000
+        rewards["total_reward"] = (sum([e for e in rewards.values()]))
+       
         if self.render_mode == "human":
             self.render()
 
@@ -182,10 +198,11 @@ class CustomEnvironment(ParallelEnv):
 
     @functools.lru_cache(maxsize=None)
     def action_space(self, agent):
-        return [0, 1, 2, 3, 4]
+        return Discrete(5)
+
 
 
 from pettingzoo.test import parallel_api_test  # noqa: E402
 
 if __name__ == "__main__":
-    parallel_api_test(CustomEnvironment(7), num_cycles=1000)
+    parallel_api_test(CustomEnvironment(7, n_drones = 5), num_cycles=1000,)
