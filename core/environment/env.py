30e69a51ed0bd27331c8a8513ca7390b65e529d0
Luis Filipe Carrete <lfcsancehz@live.com>
2023-05-02 16:43:52 -0300

    cleanup em processo

--- core/environment/env.py
+++ core/environment/env.py
@@ -13,12 +13,26 @@ from core.environment.generator.dynamic_probability import probability_matrix
 
 
 class CustomEnvironment(ParallelEnv):
-    def __init__(self, grid_size=7, render_mode="ansi", n_drones=1):
+    def __init__(self, grid_size=7, 
+                 render_mode="ansi", 
+                 render_grid = False,
+                 render_gradient = True,
+                 n_drones=1, 
+                 vector = (-0.5, -0.5), 
+                 person_initial_position = [0, 0],
+                 disperse_constant = 10):
+        
+        #Error Checking 
+        if n_drones > grid_size*grid_size:
+            raise Exception("There are more drones than grid spots. Reduce number of drones or increase grid size.")
+        if render_mode != "ansi" and render_mode != "human":
+            raise Exception("Render mode not recognized")
+        
         self.grid_size = grid_size
         self.person_y = None
         self.person_x = None
         self.timestep = None
-        self.vector = (-0.5, -0.5)
+        self.vector = vector
         self.possible_agents = []
         self.agents_positions = {}
         self.render_mode_matrix = None
@@ -26,11 +40,9 @@ class CustomEnvironment(ParallelEnv):
             self.possible_agents.append("drone" + str(i))
             self.agents_positions["drone" + str(i)] = [None, None]
 
-        self.probability_matrix = None
-
         self.render_mode = render_mode
         self.probability_matrix = probability_matrix(
-            40, 5, 5, self.vector, [self.grid_size - 1, (self.grid_size - 1)], self.grid_size
+            40, disperse_constant, disperse_constant, self.vector, person_initial_position, self.grid_size
         )
         self.map, self.person_x, self.person_y = generate_map(
             self.probability_matrix.get_matrix()
@@ -46,18 +58,24 @@ class CustomEnvironment(ParallelEnv):
         self.drone_img = None
         self.person_img = None
 
+        self.render_gradient = render_gradient
+        self.render_grid = render_grid
+
+
     def default_drones_positions(self):
         counter_x = 0
         counter_y = 0
         for i in self.agents:
+            if counter_x >= self.grid_size:
+                counter_x = 0
+                counter_y += 1
             self.agents_positions[i] = [counter_x, counter_y]
-            counter_y += 1
             counter_x += 1
 
     def required_drone_positions(self, drones_positions: list):
         for i in range(len(drones_positions)):
             x, y = drones_positions[i]
-            self.agents_positions[self.possible_agents[i]] = [y, x]
+            self.agents_positions[self.possible_agents[i]] = [x, y]
 
     def reset(self, seed=None, return_info=False, options=None, drones_positions=None):
         self.agents = copy(self.possible_agents)
@@ -65,9 +83,7 @@ class CustomEnvironment(ParallelEnv):
         self.default_drones_positions() if drones_positions is None else self.required_drone_positions(
             drones_positions
         )
-        if self.render_mode == "human-terminal":
-            self.render_terminal()
-        elif self.render_mode == "human":
+        if self.render_mode == "human":
             self.render()
 
         observations = self.create_observations()
@@ -96,8 +112,11 @@ class CustomEnvironment(ParallelEnv):
         truncations = {a: False for a in self.agents}
 
         for i in self.agents:
+            if not i in actions:
+                raise Exception("Missing action for " + i)
+            
             drone_action = actions[i]
-            # Check termination conditions
+            
             drone_x = self.agents_positions[i][0]
             drone_y = self.agents_positions[i][1]
 
@@ -159,7 +178,7 @@ class CustomEnvironment(ParallelEnv):
         # Get observations
         observations = self.create_observations()
 
-        # Get dummy infos (not used in this example)
+        # Get dummy infos 
         infos = {e: {} for e in self.agents}
 
         # CHECK COLISION
@@ -172,9 +191,7 @@ class CustomEnvironment(ParallelEnv):
                         rewards[ki] = -2000
         rewards["total_reward"] = sum([e for e in rewards.values()])
 
-        if self.render_mode == "human-terminal":
-            self.render_terminal()
-        elif self.render_mode == "human":
+        if self.render_mode == "human":
             self.render()
 
         return observations, rewards, terminations, truncations, infos
@@ -200,6 +217,7 @@ class CustomEnvironment(ParallelEnv):
             self.renderOn = True
 
     def render(self):
+        
         self.enable_render(self.render_mode)
 
         self.draw()
@@ -209,8 +227,7 @@ class CustomEnvironment(ParallelEnv):
             return
 
     def draw(self):
-        gradient = True
-        time.sleep(0.5)
+        time.sleep(0.2)
         self.screen.fill((0, 0, 0))
         drone_positions = [[x, y] for x, y in self.agents_positions.values()]
         person_position = [self.person_x, self.person_y]
@@ -225,7 +242,7 @@ class CustomEnvironment(ParallelEnv):
                 rect = pygame.Rect(x, y, self.block_size, self.block_size)
                 prob = matrix[counter_y][counter_x]
                 normalizedProb = prob/max_matrix
-                if gradient:
+                if self.render_gradient:
                     if prob == 0:
                         r, g = 255, 0
                     elif prob > 0.99:
@@ -242,7 +259,8 @@ class CustomEnvironment(ParallelEnv):
                     r, g = (0, 255) if normalizedProb >= 0.75 else  (255, 255) if normalizedProb >= 0.25 else (255, 0)
 
                 pygame.draw.rect(self.screen, (r,g,0) , rect)
-                pygame.draw.rect(self.screen, (0,0,0) , rect, 2)
+                if self.render_grid:
+                    pygame.draw.rect(self.screen, (0,0,0) , rect, 2)
 
                 if [counter_x, counter_y] in drone_positions:
                     self.screen.blit(self.drone_img, rect)
@@ -257,31 +275,6 @@ class CustomEnvironment(ParallelEnv):
             pygame.display.quit()
             self.renderOn = False
 
-    def render_terminal(self):
-        time.sleep(0.5)
-        # for windows OS
-        if os.name == "nt":
-            os.system("cls")
-
-        # for linux / Mac OS
-        else:
-            os.system("clear")
-
-        grid = np.zeros((self.grid_size, self.grid_size), dtype=object)
-
-        grid[self.person_y][self.person_x] = "X"
-        for ki, i in self.agents_positions.items():
-            grid[i[1], i[0]] = "D" + ki[-1]
-        print("----" * self.grid_size)
-        for i in grid:
-            string = "| "
-            for e in i:
-                if len(str(e)) <= 1:
-                    string += "{0} | ".format(e)
-                else:
-                    string += "{0}| ".format(e)
-            print(string)
-        print("----" * self.grid_size)
 
     def render_probability_matrix(self, mode="human-terminal"):
         if mode == "human-terminal":
