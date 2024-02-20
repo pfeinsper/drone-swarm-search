bb82f0eab1462f50b1dffb266f9cdcf8e9b05e5e
JJ <joras.custodio@gmail.com>
2024-02-20 14:26:19 -0300

    still trying to make pypi work

--- setup.py
+++ setup.py
@@ -6,13 +6,14 @@ with open("README.md", 'r') as f:
 
 
 setup(name = 'DSSE',
-      version = '0.1.17.1',
+      version = '0.1.17.7',
       description = 'An environment to train drones to search and find a shipwrecked person lost in the ocean using reinforcement learning.',
       long_description = long_description,
       long_description_content_type = 'text/markdown',
       url = 'https://github.com/pfeinsper/drone-swarm-search',
       license = 'MIT',
-      packages = ['DSSE'],
+      data_files=[('DSSE/core/environment/imgs', ['DSSE/core/environment/imgs/drone.png', 'DSSE/core/environment/imgs/person-swimming.png'])],
+      packages = ['DSSE', 'DSSE.core', 'DSSE.core.environment', 'DSSE.core.environment.generator', 'DSSE.core.environment.imgs'],
       install_requires = [
           'numpy',
           'gymnasium',
