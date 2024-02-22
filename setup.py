2382e4fd87b67f22be54569ff4d4a8ce1fabf4dd
JorasOliveira <joras.custodio@gmail.com>
2024-02-22 14:10:49 -0300

    render mode ai works fine for the pypi package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 import sys
 
 with open("README.md", 'r') as f:
@@ -6,13 +6,14 @@ with open("README.md", 'r') as f:
 
 
 setup(name = 'DSSE',
-      version = '0.1.17.7',
+      version = '0.1.17.14',
       description = 'An environment to train drones to search and find a shipwrecked person lost in the ocean using reinforcement learning.',
       long_description = long_description,
       long_description_content_type = 'text/markdown',
       url = 'https://github.com/pfeinsper/drone-swarm-search',
       license = 'MIT',
-      data_files=[('DSSE/core/environment/imgs', ['DSSE/core/environment/imgs/drone.png', 'DSSE/core/environment/imgs/person-swimming.png'])],
+      #packages = find_packages(),
+      #data_files=[('DSSE/core/environment/imgs', ['DSSE/core/environment/imgs/drone.png', 'DSSE/core/environment/imgs/person-swimming.png'])],
       packages = ['DSSE', 'DSSE.core', 'DSSE.core.environment', 'DSSE.core.environment.generator', 'DSSE.core.environment.imgs'],
       install_requires = [
           'numpy',
