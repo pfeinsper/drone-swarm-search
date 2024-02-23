f4d4105c980faef614e9c69208633d837dae0976
JorasOliveira <joras.custodio@gmail.com>
2024-02-22 22:50:34 -0300

    small changes, added manifest as atempt to inser images

--- setup.py
+++ setup.py
@@ -6,15 +6,19 @@ with open("README.md", 'r') as f:
 
 
 setup(name = 'DSSE',
-      version = '0.1.17.14',
+      version = '0.1.17.16',
+      author = "Luis Filipe Carrete, Manuel Castanares, Enrico Damiani, Leonardo Malta, Joras Oliveira, Ricardo Ribeiro Rodrigues, Renato Lafrachi Falcao, Pedro Andrade, Fabricio Barth",
       description = 'An environment to train drones to search and find a shipwrecked person lost in the ocean using reinforcement learning.',
       long_description = long_description,
       long_description_content_type = 'text/markdown',
       url = 'https://github.com/pfeinsper/drone-swarm-search',
       license = 'MIT',
-      #packages = find_packages(),
+      packages = find_packages(),
+      package_data={
+        'DSSE': ['core/environment/imgs/.png', 'core/environment/imgs/.jpg'],
+        },
       #data_files=[('DSSE/core/environment/imgs', ['DSSE/core/environment/imgs/drone.png', 'DSSE/core/environment/imgs/person-swimming.png'])],
-      packages = ['DSSE', 'DSSE.core', 'DSSE.core.environment', 'DSSE.core.environment.generator', 'DSSE.core.environment.imgs'],
+      #packages = ['DSSE', 'DSSE.core', 'DSSE.core.environment', 'DSSE.core.environment.generator', 'DSSE.core.environment.imgs'],
       install_requires = [
           'numpy',
           'gymnasium',
