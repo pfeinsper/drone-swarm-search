31f9a2d2100d93fd932bfaa7f208d2f63fe7d622
Pedro2712 <pedrohenriqueandrade2712@gmail.com>
2024-03-24 14:18:07 -0300

    ajuste FPS

--- DSSE/environment/pygame_interface.py
+++ DSSE/environment/pygame_interface.py
@@ -10,7 +10,7 @@ class PygameInterface:
     Class for rendering the grafical interface of the simulation
     """
 
-    FPS = 3
+    FPS = 15
 
     def __init__(
         self, grid_size: int, render_gradient: bool, render_grid: bool
