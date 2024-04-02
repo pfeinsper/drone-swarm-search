740b1c6fad34a537413f031688c7316f36c21296
Pedro2712 <pedrohenriqueandrade2712@gmail.com>
2024-04-02 19:36:11 -0300

    Fix reset

--- basic_env.py
+++ basic_env.py
@@ -8,7 +8,7 @@ env = DroneSwarmSearch(
     render_gradient=True,
     vector=(3.2, 3.1),
     disperse_constant=5,
-    timestep_limit=200,
+    timestep_limit=10,
     person_amount=5,
     person_initial_position=(10, 10),
     drone_amount=2,
@@ -25,8 +25,18 @@ def policy(obs, agents):
     return actions
 
 
-observations = env.reset(drones_positions=[(0, 10), (0, 11)])
+observations = env.reset(drones_positions=[(10, 10), (0, 11)])
 
+rewards = 0
+done = False
+while not done:
+    actions = policy(observations, env.get_agents())
+    observations, reward, _, done, info = env.step(actions)
+    rewards += reward["total_reward"]
+    done = any(done.values())
+    
+    
+observations = env.reset(drones_positions=[(0, 0), (0, 11)])
 rewards = 0
 done = False
 while not done:
